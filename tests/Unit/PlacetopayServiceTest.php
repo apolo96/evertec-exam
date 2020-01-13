359f95650ac6a72f9b9a61ea466e6d3b0f501015
Andres Polo <andresfpvclap@gmail.com>
2020-01-13 10:38:26 -0500

    Add test case to placetopay service

--- tests/Unit/PlacetopayServiceTest.php
+++ tests/Unit/PlacetopayServiceTest.php
@@ -38,15 +38,7 @@ class PlacetopayServiceTest extends TestCase
      */
     public function it_should_payment_request_with_success()
     {
-        $payData = [
-            'reference' => '1111111',
-            'description' => 'Testing payment',
-            'amount' => [
-                'currency' => 'COP',
-                'total' => 200000,
-            ],
-        ];
-        $sessionPayment = $this->placetopay->payment($payData);
+        $sessionPayment = $this->make_payment_request();
         $this->assertTrue($sessionPayment->isSuccessful());
         $this->assertNotEmpty($sessionPayment->requestId());
         $this->assertNotEmpty($sessionPayment->processUrl());
@@ -58,10 +50,44 @@ class PlacetopayServiceTest extends TestCase
      * @return void
      * @test
      */
-    public function it_should_check_status_pay()
+    public function it_should_check_status_pay_with_invalid_id()
     {
+        
         $response = $this->placetopay->payInfo("121765");
         $this->assertFalse($response->isSuccessful());
         $this->assertFalse($response->status()->isApproved());
+    }
+
+    /**
+     * Unit test: service response with valid id on request information
+     *
+     * @return void
+     * @test
+     */
+    public function it_should_check_status_pay()
+    {
+        $sessionPayment = $this->make_payment_request();
+        $response = $this->placetopay->payInfo($sessionPayment->requestId());
+        $this->assertTrue($response->isSuccessful());
+        $this->assertNotEmpty($response->status());
+    }
+
+    /**
+     * Support Unit test: make payment request
+     *
+     * @return RedirectInformation
+     * 
+     */
+    public function make_payment_request()
+    {
+        $payData = [
+            'reference' => '1111111',
+            'description' => 'Testing payment',
+            'amount' => [
+                'currency' => 'COP',
+                'total' => 200000,
+            ],
+        ];
+        return $this->placetopay->payment($payData);
     }
 }
