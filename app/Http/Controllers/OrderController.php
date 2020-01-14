7103bf9b2b52324bebdf26e1c6bfed5beefd342f
Andres Polo <andresfpvclap@gmail.com>
2020-01-14 13:56:51 -0500

    Order all orders-model by datetime

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -29,7 +29,7 @@ class OrderController extends Controller
      */
     public function index()
     {
-        $orders = Order::all();
+        $orders = Order::orderBy('created_at', 'desc')->get();
         return view('order.index', compact('orders'));
     }
 
